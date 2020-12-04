010cd909ccc5d5b61f314577a3e8eb7af0486f75
Ankur Handa <ahanda@nvidia.com>
2020-12-03 19:34:39 -0800

    adding interactive image plotting
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- interactive_img_plot.py
+++ interactive_img_plot.py
@@ -2,17 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from PIL import Image
 
-
 img = Image.open('img_data/img_0000.jpg')
 
 img_plot = plt.imshow(img)
 
 for i in range(1,79):
-
-        
+       
     img = Image.open('img_data/img_{:04d}.jpg'.format(i))
     img_plot.set_data(img)
     plt.pause(0.01)
     
-
 plt.show()
